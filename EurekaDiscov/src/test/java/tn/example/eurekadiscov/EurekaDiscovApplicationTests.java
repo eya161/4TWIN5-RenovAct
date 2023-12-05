d6639cf97b961db6b66d10b88e5e17aad1b8f0ee
eya161 <araari.eya@gmail.com>
2023-12-05 20:38:25 +0100

    Eureka

---
+++ EurekaDiscov/src/test/java/tn/example/eurekadiscov/EurekaDiscovApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.example.eurekadiscov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
